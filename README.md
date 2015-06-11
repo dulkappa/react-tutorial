0ec9027ef085662d0d4061a3b31e335e5159adb9
dulkappa <dulkappa@gmail.com>
2015-06-11 22:32:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React.js tutorial
+React.jsの[チュートリアル](https://facebook.github.io/react/docs/tutorial.html)を触ってみた軌跡.
+
+## メモ
+- 最小構成でgulpとか使えるようにした.(2015-06-11 22:12)
+- とりあえず入門.(2015-06-09 23:58)
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var react = require('gulp-react');
+var using = require('gulp-using'); // ファイル名の出力
+var webserver = require('gulp-webserver');
+var plumber = require('gulp-plumber'); // エラー発生時もタスクを継続する
+
+gulp
+  .task('webserver', function() {
+    gulp
+      .src('./')
+      .pipe(webserver({
+        livereload: true,
+        open: true,
+        port: 9999
+      }));
+  });
+
+gulp.task('react', function() {
+  return gulp.src('./jsx/**/*.jsx')
+    .pipe(plumber())
+    .pipe(using())
+    .pipe(react())
+    .pipe(gulp.dest('js/'));
+});
+
+gulp.task('watch', ['webserver'], function() {
+  gulp.watch(['./jsx/**/*.jsx'], ['react']);
+});
