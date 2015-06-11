939b08e84c5c9b3a6768d30baa569015179e539e
dulkappa <dulkappa@gmail.com>
2015-06-11 23:16:08 +0900

    separate data from component

--- jsx/commentBox.jsx
+++ jsx/commentBox.jsx
@@ -1,9 +1,25 @@
+var Comment = React.createClass({
+  render: function () {
+    var rawMarkup = marked(this.props.children.toString(), {
+      sanitize: true
+    });
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}}/>
+      </div>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
   render: function () {
     return (
       <div className='commentBox'>
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -12,9 +28,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function () {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    })
     return (
       <div className='commentList'>
-        Hello, world! I am CommentList.
+        {commentNodes}
       </div>
     );
   }
@@ -30,4 +53,14 @@ var CommentForm = React.createClass({
   }
 });
 
-React.render(<CommentBox />, document.getElementById('content'));
+var data = [
+  {
+    author: "Pete Hunt",
+    text: "This is **one** comment"
+  }, {
+    author: "Jordan Walke",
+    text: "This is *another* comment"
+  }
+];
+
+React.render(<CommentBox data={data} />, document.getElementById('content'));
